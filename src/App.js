09a062ac2c91ed31a86ee71e4377751234a28378
streeptococcus <anapalmeidaj1@gmail.com>
2020-07-28 23:24:36 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Menu from './components/Menu'
+import dadosIniciais from './data/dados_iniciais.json'
+import Carousel from './components/Carousel'
+import BannerMain from './components/BannerMain'
+import Footer from './components/Footer'
+
+function App() {
+    return ( <
+        div >
+        <
+        Menu / >
+
+
+
+        <
+        Footer / >
+        <
+        /div>
+    );
+}
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Logo from '../../assets/img/logo.png'
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="https://www.youtube.com/channel/UCwbyt6iJGbTE1Qj3E3Pio1w">
+        <img src={Logo} alt="VanityFlix" />
+      </a>
+      <p>
+        Orgulhosamente criado durante a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          Imersão React da Alura
+        </a>
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
