0cfde8a84c238dbad4ca3148659de8cdcd619132
streeptococcus <anapalmeidaj1@gmail.com>
2020-07-29 16:01:20 -0300

    Routing and adding carousel animation

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
 import Logo from '../../assets/img/logo.png'
 import './Menu.css'
 import Button from '../Button'
 
-//import ButtonLink from '../components/ButtonLink';
-
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/"> 
+      <Link to="/"> 
       <img className="Logo" src={Logo} alt="FatecFlix" />
-      </a>
-      <Button as="a" className="ButtonLink" href="/">Novo Vídeo</Button>
+      </Link>
+      <Button as={Link} className="ButtonLink" to="/cadastroVideo">Novo Vídeo</Button>
       </nav>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import './index.css'
+import Home from './pages/Home'
+import CadastroVideo from './pages/Cadastro/Video'
+import CadastroCategoria from './pages/Cadastro/Categoria'
+
+const Pagina404 = () => (<div>Página 4040</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' component={Home} exact />
+      <Route path='/cadastroVideo' component={CadastroVideo} exact />
+      <Route path='/cadastroCategoria' component={CadastroCategoria} exact />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
-);
+)
